a3a09b099f74552b1b5339ef2f0e6c04d0477764
Nebi-k <misternebi@gmail.com>
2020-07-17 14:51:08 +0200

    remote client server

--- spytank/net_remote/client.py
+++ spytank/net_remote/client.py
@@ -3,5 +3,28 @@ from gtts import gTTS
 import os
 import network
 
-ADDRESS = "10.0.0.xxx"
+ADDRESS = "10.0.0.111"
 PORT = 1111
+
+z= "z: avancer"
+s= "s: reculer"
+q= "q: tourner a gauche"
+d= "d: tourner a droite"
+x= "x: stop"
+a= "a: augmenter vitesse"
+e= "e: diminuer la vitesse"
+p= "p: quitter"
+
+print(z,s,q,d,x,a,e,p)
+
+continuer = True
+while continuer:
+    socket = network.newClientSocket()
+    socket.connect((ADDRESS, PORT))
+
+    print("tapez votre lette")
+    lettre = click.getchar()
+
+    socket.send(lettre.encode())
+    if lettre == "p":
+        continuer = False
